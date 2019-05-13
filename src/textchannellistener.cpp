ab7d1a47f2e180a42fae3ce64d74a1c45716aa25
Alexander Akulich <akulichalexander@gmail.com>
2019-05-13 23:52:15 +0300

    [commhistoryd] Process message delivery status on message received
    In case of IM it is possible to receive an already read message.

--- src/textchannellistener.cpp
+++ src/textchannellistener.cpp
@@ -1203,6 +1203,10 @@ void TextChannelListener::handleReceivedMessage(const Tp::ReceivedMessage &messa
         event.setDirection(CommHistory::Event::Inbound);
     }
 
+    Tp::DeliveryStatus deliveryStatus = message.deliveryDetails().status();
+    if (deliveryStatus == Tp::DeliveryStatusRead)
+        event.setIsRead(true);
+
     QDateTime receivedTime;
     if (message.received().isValid())
         receivedTime = message.received();
